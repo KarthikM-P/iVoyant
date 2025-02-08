d5a4f29cfca81e2a730f74edc84110a3c5d6cc99
Karthik mp <130955414+KarthikM-P@users.noreply.github.com>
2025-02-08 18:56:54 +0530

    add jsx props file

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+import Child from "./Child"
+
+const App = () => {
+  return (
+    <>
+      <Child>
+        <h1>jsx as a props </h1>
+        <p>this heading and paragraph is sent as a paragraph</p>
+      </Child>
+    </>
+  )
+}
+
+export default App

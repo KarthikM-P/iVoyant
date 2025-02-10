46cab26a0ac173e265cf47e954cd2b64b8f66155
Karthik mp <130955414+KarthikM-P@users.noreply.github.com>
2025-02-10 18:13:26 +0530

    add new usestate project with search

--- src/Products.tsx
+++ src/Products.tsx
@@ -20,7 +20,7 @@ const Products = () => {
     <div>
       <h1>Products</h1>
       <input type="text" onChange={search} placeholder="Search by category" />
-      
+      <h1>{searchTerm}</h1>
         {productinfo.map((product) => (
           <div key={product.id}>
             <h2>{product.name}</h2>

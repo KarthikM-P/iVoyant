7627bc0d762734a1f8141b827d644e9b17656b2c
Karthik mp <130955414+KarthikM-P@users.noreply.github.com>
2025-02-10 18:11:35 +0530

    add new usestate project

---
+++ src/Products.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import products from './product.json';
+
+const Products = () => {
+  const [productinfo, setProductInfo] = useState(products);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const search = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value.toLowerCase();
+    setSearchTerm(value);
+
+    const filteredProducts = products.filter((product) =>
+      product.category.toLowerCase().includes(value)
+    );
+
+    setProductInfo(filteredProducts);
+  };
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <input type="text" onChange={search} placeholder="Search by category" />
+      
+        {productinfo.map((product) => (
+          <div key={product.id}>
+            <h2>{product.name}</h2>
+            <p>Price: ${product.price}</p>
+            <p>Category: {product.category}</p>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+
+export default Products;
